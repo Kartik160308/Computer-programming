93b5ebeb35a396742b19ec629a44e93e2c12ee66
Kartik160308 <kartik.n.solanki16@gmail.com>
2025-08-25 15:33:52 +0530

    Add files via upload

---
+++ 3(2).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+
+{
+    int i;
+    i=1;
+    do
+    {
+        i+=2;
+        printf("%d\n", i);
+
+    }
+    while(i<20);
+}
---
+++ 3(3).c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+
+{
+    int i,n;
+    i=0;
+    do
+    {
+        i+=2;
+        printf("%d\n", i);
+
+    }
+    while(i<n);
+}
+
