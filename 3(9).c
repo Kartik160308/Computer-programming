726adb08e4c39151aaaa1413920508e2ed727c92
Kartik <kartik.n.solanki16@gmail.com>
2025-09-01 15:03:41 +0530

    Add files via upload

---
+++ 3(9).c
@@ -0,0 +1,19 @@
+
+
+#include<stdio.h>
+void main()
+
+{
+    int n,sum;
+    sum=0;
+
+    printf("Enter how many odd numbers to sum:");
+    scanf("%d", &n);
+
+    for (int i=1;i<=n;i++){sum +=2*i;}
+
+    printf("Sum of first %d\n odd numbers is:%d\n", n,sum);
+
+}
+
+
